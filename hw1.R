6f11a5571994982f1a4e187d5dc771e3316809ca
LarryinMexico <116884263+LarryinMexico@users.noreply.github.com>
2024-03-07 16:54:33 +0800

    Update hw1.R

--- hw1.R
+++ hw1.R
@@ -3,9 +3,9 @@ summary <- function(input_path, output_path){
   length <- nchar(input_path)-4
   max_weight <- round(max(input$weight, na.rm = TRUE), digits = 2)
   max_height <- round(max(input$height, na.rm = TRUE), digits = 2)
-  result <- data.frame(set = paste0("input",substr(input_path, length, length)), 
+  result <- data.frame(set = paste0("input", substr(input_path, length, length)), 
                      weight = max_weight, 
                      height = max_height)
-  write.csv(result, file = output_path)
+  write.csv(result, file = output_path, row.names = FALSE)
 }
 #summary("./example/input1.csv", "./example/output1_test.csv")
