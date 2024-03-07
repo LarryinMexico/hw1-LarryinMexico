7c5ae1fad9be577ffa769512de3a51b0a2b033d2
LarryinMexico <116884263+LarryinMexico@users.noreply.github.com>
2024-03-07 16:40:50 +0800

    Update hw1.R

--- hw1.R
+++ hw1.R
@@ -1,10 +1,9 @@
 summary <- function(input_path, output_path){
   input <- read.csv(input_path)
-  length <- nchar(input_path)
-  set_name <- substr(input_path, length-4, length-4)
+  length <- nchar(input_path)-4
   max_weight <- round(max(input$weight, na.rm = TRUE), digits = 2)
   max_height <- round(max(input$height, na.rm = TRUE), digits = 2)
-  result <- data.frame(set = paste0("input",set_name), 
+  result <- data.frame(set = paste0("input",substr(input_path, length, length)), 
                      weight = max_weight, 
                      height = max_height)
   write.csv(result, file = output_path, row.names = FALSE)
