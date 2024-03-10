6f70c9bad49b9cdc3f1e3acf00e95b0c84172e28
LarryinMexico <116884263+LarryinMexico@users.noreply.github.com>
2024-03-10 11:23:12 +0800

    Update hw1.R
    change the way to extract name from the path name

--- hw1.R
+++ hw1.R
@@ -1,11 +1,12 @@
 summary <- function(input_path, output_path){
   input <- read.csv(input_path)
-  length <- nchar(input_path)-4
   max_weight <- round(max(input$weight, na.rm = TRUE), digits = 2)
   max_height <- round(max(input$height, na.rm = TRUE), digits = 2)
-  result <- data.frame(set = paste0("input", substr(input_path, length, length)), 
-                     weight = max_weight, 
-                     height = max_height)
-  write.csv(result, file = output_path, row.names = FALSE, quote = FALSE)
+  result <- data.frame(set = sub('\\.csv$', '', basename(input_path)), 
+                       weight = max_weight, 
+                       height = max_height)
+  write.csv(result, file = output_path, quote = FALSE, row.names = FALSE)
 }
-#summary("./example/input1.csv", "./example/output1_test.csv")
+
+summary("./example/input1.csv", "./example/output1_test1.csv")
+
